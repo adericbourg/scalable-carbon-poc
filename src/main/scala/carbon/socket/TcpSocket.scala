9f3506dccba8253b629ad780ec9f0b5ad669ff49
Alban Dericbourg <alban@dericbourg.net>
2019-01-23 16:35:12 +0100

    Move back handlers in their associated socket definition
    Signed-off-by: Alban Dericbourg <a.dericbourg@criteo.com>

--- src/main/scala/carbon/socket/TcpSocket.scala
+++ src/main/scala/carbon/socket/TcpSocket.scala
@@ -2,12 +2,13 @@ package carbon.socket
 
 import java.util
 
-import carbon.EnqueueTcpMessageHandler
 import carbon.model.Message
 import io.netty.bootstrap.ServerBootstrap
-import io.netty.channel.ChannelInitializer
+import io.netty.buffer.ByteBuf
 import io.netty.channel.nio.NioEventLoopGroup
 import io.netty.channel.socket.nio.{NioServerSocketChannel, NioSocketChannel}
+import io.netty.channel.{ChannelHandlerContext, ChannelInboundHandlerAdapter, ChannelInitializer}
+import io.netty.util.CharsetUtil
 
 class TcpSocket(port: Int, inboundQueue: util.Queue[Message]) extends CarbonSocket {
   private val workerGroup = new NioEventLoopGroup()
@@ -17,7 +18,7 @@ class TcpSocket(port: Int, inboundQueue: util.Queue[Message]) extends CarbonSock
     .channel(classOf[NioServerSocketChannel])
     .childHandler(new ChannelInitializer[NioSocketChannel] {
       override def initChannel(ch: NioSocketChannel): Unit = {
-        ch.pipeline().addLast(new EnqueueTcpMessageHandler(inboundQueue))
+        ch.pipeline().addLast(new TcpSocketMessageHandler(inboundQueue))
       }
     })
 
@@ -30,5 +31,14 @@ class TcpSocket(port: Int, inboundQueue: util.Queue[Message]) extends CarbonSock
       workerGroup.shutdownGracefully().sync()
       println("TCP socket gracefully shutdown :)")
     }
+  }
+}
+
+
+class TcpSocketMessageHandler(queue: util.Queue[Message]) extends ChannelInboundHandlerAdapter {
+  override def channelRead(ctx: ChannelHandlerContext, msg: Any): Unit = {
+    val raw = msg.asInstanceOf[ByteBuf].toString(CharsetUtil.UTF_8)
+    val message = Message(raw)
+    queue.add(message)
   }
 }
--- src/main/scala/carbon/socket/UdpSocket.scala
+++ src/main/scala/carbon/socket/UdpSocket.scala
@@ -1,13 +1,16 @@
 package carbon.socket
 
+import java.util
 import java.util.concurrent.BlockingQueue
 
-import carbon.EnqueueMessageHandler
 import carbon.model.Message
 import io.netty.bootstrap.Bootstrap
-import io.netty.channel.ChannelInitializer
 import io.netty.channel.nio.NioEventLoopGroup
+import io.netty.channel.socket.DatagramPacket
 import io.netty.channel.socket.nio.NioDatagramChannel
+import io.netty.channel.{ChannelHandlerContext, ChannelInitializer, SimpleChannelInboundHandler}
+import io.netty.util.CharsetUtil
+
 
 class UdpSocket(port: Int, inboundQueue: BlockingQueue[Message]) extends CarbonSocket {
 
@@ -18,7 +21,7 @@ class UdpSocket(port: Int, inboundQueue: BlockingQueue[Message]) extends CarbonS
     .channel(classOf[NioDatagramChannel])
     .handler(new ChannelInitializer[NioDatagramChannel] {
       override def initChannel(ch: NioDatagramChannel): Unit = {
-        ch.pipeline().addLast(new EnqueueMessageHandler(inboundQueue))
+        ch.pipeline().addLast(new UdpSocketMessageHandler(inboundQueue))
       }
     })
 
@@ -31,5 +34,13 @@ class UdpSocket(port: Int, inboundQueue: BlockingQueue[Message]) extends CarbonS
       workerGroup.shutdownGracefully().sync()
       println("UDP socket gracefully shutdown :)")
     }
+  }
+}
+
+class UdpSocketMessageHandler(queue: util.Queue[Message]) extends SimpleChannelInboundHandler[DatagramPacket] {
+  override def channelRead0(ctx: ChannelHandlerContext, msg: DatagramPacket): Unit = {
+    val raw = msg.content().toString(CharsetUtil.UTF_8)
+    val message = Message(raw)
+    queue.add(message)
   }
 }
