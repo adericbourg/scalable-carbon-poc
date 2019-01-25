696867344f14ba36c0e458da2bbd79b4a7ee3993
Alban Dericbourg <alban@dericbourg.net>
2019-01-25 16:32:19 +0100

    Use parser combinators to parse input data
    Signed-off-by: Alban Dericbourg <alban@dericbourg.net>

--- build.sbt
+++ build.sbt
@@ -7,5 +7,7 @@ scalaVersion := "2.12.8"
 libraryDependencies += "com.typesafe" % "config" % "1.3.2"
 libraryDependencies += "io.netty" % "netty-all" % "4.1.32.Final"
 
+libraryDependencies += "com.lihaoyi" %% "fastparse" % "2.1.0"
+
 libraryDependencies += "org.scalactic" %% "scalactic" % "3.0.5"
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.5" % "test"
--- src/main/scala/carbon/parsing/MetricParser.scala
+++ src/main/scala/carbon/parsing/MetricParser.scala
@@ -1,24 +1,39 @@
 package carbon.parsing
 
 import carbon.model.Message
-
-import scala.util.Try
+import fastparse.NoWhitespace._
+import fastparse._
 
 object MetricParser {
+
   def parse(in: String): Seq[Message] = in.split("\n").flatMap(parseOne)
 
   def parseOne(in: String): Option[Message] = {
-    in.split(" ") match {
-      case Array(metricName, value, timestamp) => buildMessage(metricName, value, timestamp)
-      case _                                   => None
+    fastparse.parse(in, message(_)) match {
+      case Parsed.Success(result, _) => Some(result)
+      case _                         => None // TODO log.debug error
     }
   }
 
-  private def buildMessage(values: (String, String, String)): Option[Message] = {
-    val (metricName, value, timestamp) = values
-    Try {
-      Message(metricName, value.toDouble, timestamp.toInt)
-    }.toOption
-  }
-}
+  private def message[_: P]: P[Message] = P(metricName ~ space ~ value ~ space ~ timestamp ~ End)
+    .map(tuple => Message(tuple._1, tuple._2, tuple._3))
 
+  private def space[_: P]: P[Unit] = P(CharIn(" ").rep(1))
+
+  private def metricName[_: P]: P[String] = P(((CharIn("a-zA-Z").rep(1) ~ ".").rep(0) ~ CharIn("a-zA-Z").rep(1)).!)
+
+  private def timestamp[_: P]: P[Long] = P(sign ~ digit.rep(1).!).map(_.toLong)
+
+  private def value[_: P]: P[Double] = {
+    def withPoint = P(digit.rep(0) ~ "." ~ digit.rep(1))
+
+    def withoutPoint = P(digit.rep(1))
+
+    P((sign ~ (withPoint | withoutPoint)).!).map(_.toDouble)
+  }
+
+  private def sign[_: P] = P(CharIn("+\\-").?)
+
+  private def digit[_: P] = P(CharIn("0-9"))
+
+}
